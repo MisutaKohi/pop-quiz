1f646f912e3880fbbaa94af86d040a0471a787f5
Ryan Morgan <R.Morgan5399@gmail.com>
2022-05-18 06:11:37 -0700

    begin implementing main method

--- pop_quiz.py
+++ pop_quiz.py
@@ -1,5 +1,31 @@
+'''This module represents a multiplication pop-quiz. Using Python's
+PyInputPlus and Time libraries, the script will prompt a user to
+answer a series of 10 questions. They will have 3 chances and 10
+seconds to answer each question. The questions will be randomly
+generated for the quiz each time from 1-12 inclusive. Afterward,
+the user's name, the generated questions, and the user's score
+will be exported to a txt file as a souvenir.'''
+
 import pyinputplus as pyip
+import time, random
 
-inpt = pyip.inputNum('Enter a num:\n')
+def main():
 
-print(inpt)
+    results = []
+    quiz_questions = 10
+    correct_answers = 0
+
+    for question in range(quiz_questions):
+
+        num1 = random.randint(1, 12) #inclusive range
+        num2 = random.randint(1, 12)
+
+        answer = pyip.inpu
+
+
+
+
+
+
+
+#file = open('certificate.txt', 'w')
